c21b8860dc553368799c2fd5b6768ead820e1e8f
brieuc <pinonbrieuc@gmail.com>
2018-12-08 15:08:06 +0100

    adaptative lambda

--- Deconvolution_lambda.py
+++ Deconvolution_lambda.py
@@ -14,18 +14,7 @@ def SSD(x,y):
     """ Sum of square differences """
     return np.sum((x-y)**2)
 
-def cost(f0, fSpars, Phi):
-	#Blind Deconvolution Using a Normalized Sparsity Measure by Krishnan, Tay and Fergus
-	#L1/L2
-    Grad = lambda f: (np.abs(f[:,0:-1]-f[:,1:]), np.abs(f[0:-1,:]-f[1:,:]))
-    G = Grad(fSpars)#gradient of the image solution along x and y
-    L1 = np.sum(np.abs(G[0]))+np.sum(np.abs(G[1]))
-    L2 = np.sqrt(np.sum(G[0]**2)+np.sum(G[1]**2))
-
-    return L1/L2
-
 def correlation_transform(f,radius):
-    # use it to compute an approximate measure of local correlation
     n = max(f.shape);
     t = np.concatenate( (np.arange(0,n/2+1), np.arange(-n/2,-1)) )
     [Y,X] = np.meshgrid(t,t)
@@ -41,7 +30,17 @@ def whiteness(r):
     r_norm = r/np.var(r)
     return 1e-6*np.abs(np.sum(np.multiply(r_norm, correlation_transform(r_norm, 10))))
 
-def deconvolution(f, kernel, scale, options, lmbd=0.01):
+def cost(f0, fSpars, Phi):
+	#Blind Deconvolution Using a Normalized Sparsity Measure by Krishnan, Tay and Fergus
+	#L1/L2
+    Grad = lambda f: (np.abs(f[:,0:-1]-f[:,1:]), np.abs(f[0:-1,:]-f[1:,:]))
+    G = Grad(fSpars)#gradient of the image solution along x and y
+    L1 = np.sum(np.abs(G[0]))+np.sum(np.abs(G[1]))
+    L2 = np.sqrt(np.sum(G[0]**2)+np.sum(G[1]**2))
+
+    return L1/L2
+
+def deconvolution_adaptativeLambda(f, kernel, scale, options):
 
     # define wavelet transform and inverse wavelet transform functions
     PsiS = lambda f: perform_wavelet_transf(f,3, +1, ti=1)
@@ -54,39 +53,43 @@ def deconvolution(f, kernel, scale, options, lmbd=0.01):
     #the linear blur operation
     Phi = lambda x: kernel(x, scale)
 
-    #optimization
-    #lmbd = 0.01
+    lmbds = np.exp(np.linspace(-11,-2,10))
+    W = np. zeros(lmbds.shape)
+    for i,lmbd in enumerate(lmbds):
+        #optimization
+        #lmbd = 0.01
+        tau = 1.5
+        niter = 20
+        a = PsiS(f0)#np.zeros(PsiS(f0).shape)
+        for iter in range(niter):
+            #if(options['verbose']): print(cost(f0, Psi(a), Phi))
+
+            #gradient step
+            a = np.add(a, tau*PsiS(Phi(f0-Phi(Psi(a)))))
+            #soft-threshold step
+            a = SoftThresh(a, lmbd*tau )
+
+            #imageplot(Psi(a)-f0, 'Image')
+            #plt.show()
+
+        W[i] = whiteness(Phi(Psi(a))-f0)
+        print("Lambda : " + str(lmbd))
+        print("Residual whiteness : " + str(whiteness(Phi(Psi(a))-f0)))
+        print()
+            #print("SSD : " + str(SSD(Phi(Psi(a)),f0)))
+
+    lmbd = lmbds[np.argmin(W)]
+    print("Choosen lambda : "+str(lmbd))
     tau = 1.5
     niter = 20
-    a = PsiS(f0)
+    a = PsiS(f0)#np.zeros(PsiS(f0).shape)
     for iter in range(niter):
-        #if(options['verbose']): print(cost(f0, Psi(a), Phi))
-
         #gradient step
         a = np.add(a, tau*PsiS(Phi(f0-Phi(Psi(a)))))
         #soft-threshold step
         a = SoftThresh(a, lmbd*tau )
 
     return Psi(a)
-
-def deconvolution_adaptativeLambda(f, kernel, scale, options):
-    """
-        make a search on the lambda value to get a residual as decorrelated as possible
-        Parameter estimation for blind and nonblind deconvolution using residual whiteness measures
-    """
-    lmbds = np.exp(np.linspace(-11,-2,10))
-    W = np. zeros(lmbds.shape)
-    for i,lmbd in enumerate(lmbds):
-        fSpars = deconvolution(f, kernel, scale, options, lmbd=lmbd)
-        W[i] = whiteness(kernel(fSpars, scale)-f0)
-        print("Lambda : "+str(lmbd)+"\tWhiteness : "+str(W[i]))
-
-    lmbd = lmbds[np.argmin(W)]
-    if(options['verbose']): print("Choosen lambda : "+str(lmbd))
-
-    fSpars = deconvolution(f, kernel, scale, options, lmbd=lmbd)
-
-    return fSpars
 
 def deconvolution_unknown_scale(f, kernel, options):
     scales = np.linspace(1,10,10)
@@ -128,7 +131,7 @@ def circular_blur(f,radius):
 
 
 f0 = load_image("DFB_artificial_dataset/im5_blurry.bmp")
-
+print(whiteness(f0))
 options = {}
 options['verbose'] = True
 
